2470a3787af2329528851dfab032733b2166b38a
abhinav <uniqueabhinavgupta@gmail.com>
2023-04-07 20:01:57 -0400

    Updated Binary Search Program

---
+++ src/ErrorsExample.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ErrorsExample {
+    public static void main(String[] args) {
+
+        // Syntax errors
+        //int a = 5
+        //System.out.println("Value of a is " + a);
+
+        //Logical errors
+        //Print Prime numbers from 1 to 10
+        /*System.out.println(2);
+        for (int i=1; i<10;i++){
+            System.out.println(2*i+1);
+        }*/
+
+        //Run time errors or exceptions
+        /*Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("The value of number divided by 1111 is " + 1111/a);*/
+
+
+    }
+}
